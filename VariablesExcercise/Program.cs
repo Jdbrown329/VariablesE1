93a6b6d082f49cfc6ccf0ebe9b73187d2dc74485
Jordan Brown <j.d.brown329@gmail.com>
2022-09-12 13:32:53 -0500

    First committ

---
+++ VariablesExcercise/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+String firstName = "Jordan";
+int age = 32;
+Char middleInitial = 'd';
+bool hasDriversliscence = true;
+double dateRating = 9.5;
+decimal bankBalance = 2000.65m;
+Console.WriteLine($"I am {firstName}, I've lived for {age} and have a middle initial of {middleInitial}. Can I legally drive? {hasDriversliscence}- Partners have rated me about a {dateRating}. I currently have {bankBalance} dollars in my bank account.");
